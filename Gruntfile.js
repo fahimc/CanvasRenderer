a137fb9260272df84479f4e6759031c187f31dfa
unknown <Fahim@Fahim-PC.(none)>
2013-10-27 18:07:40 +0000

    created jsdocs and gitpages

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,29 +4,13 @@ module.exports = function(grunt) {
 		pkg : grunt.file.readJSON('package.json'),
 		jsdoc : {
 			dist : {
-				src : ['app/lib/canvas/*.js','README.md'],
-				dest : 'doc',
-				options:{
-					template:'template/default'
+				src : ['app/lib/canvas/*.js', 'README.md'],
+				dest : 'pages',
+				options : {
+					template : 'template/default'
 				}
 			}
 		},
-		html2md: {
-    main: {
-      options: {
-        encoding: 'utf8'
-      },
-      src: [
-        'doc/*.html'
-      ]
-    },
-  },
-  copy: {
-  main: {
-    src: 'doc/*.md',
-    dest: 'wiki/',
-  },
-},
 		concat : {
 			options : {
 				stripBanners : true,
@@ -52,9 +36,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-jsdoc');
-	grunt.loadNpmTasks('grunt-html2md');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-github-pages');
 	// A very basic default task.
-	grunt.registerTask('default', ["concat", "uglify", "jsdoc","html2md","copy"]);
+	grunt.registerTask('default', ["concat", "uglify", "jsdoc"]);
 
 };
